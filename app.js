a6e5f7b131ab85cf55cbb67b7777e3e8bbf81e61
Tim Trautman <tctrautman@gmail.com>
2017-12-14 16:49:28 -0800

    Add stubs for search and view product page

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+// Home page
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Search
+// To do: add search term to be dynamically put in after keywords
+app.get('/s/field-keywords=', (req, res) => {
+
+});
+
+// View product page
+// To do: add product id after the slash
+app.get('/product/', (req, res) => {
+
+});
 
 app.listen(7331, () => console.log('Amazon Client app listening on port 3000!'));
